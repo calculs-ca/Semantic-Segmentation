28978ee2246621374e37c77a8521089d1faea1ef
aklopezcarbajal <aklopezcarbajal@gmail.com>
2021-10-22 14:56:34 -0400

    Sort images by name. Add function to show images

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 import os
 import cv2
+import torch
+import matplotlib.pyplot as plt
+from natsort import natsorted
 
 def load_images(path):
     images = []
-    for filename in os.listdir(path):
+    img_list = natsorted(os.listdir(path))
+    for filename in img_list:
         img = cv2.imread(os.path.join(path, filename))
         if img is not None:
             images.append(img)
     return images
+
+def imshow(img):
+    img = torch.squeeze(img)
+    plt.imshow(img.numpy())
+    plt.show()
