7e1f9b892f2c314292807ecf1b80c626fe1a4a14
aklopezcarbajal <aklopezcarbajal@gmail.com>
2021-10-22 14:21:23 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+
+def load_images(path):
+    images = []
+    for filename in os.listdir(path):
+        img = cv2.imread(os.path.join(path, filename))
+        if img is not None:
+            images.append(img)
+    return images
