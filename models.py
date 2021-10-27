78d604e1c6e6ea6778a7e15e3268f95aa8e010e7
aklopezcarbajal <aklopezcarbajal@gmail.com>
2021-10-27 12:42:47 -0400

    Working on fully-conv model

--- models.py
+++ models.py
@@ -9,19 +9,32 @@ Classes: 8, BW-000, HD-001, PF-010, WR-011, RO-100, RI-101, FV-110, SR-111
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.features = [4, 8, 16]
+        self.features = [8, 16, 32]
         self.encoder = nn.ModuleList()
         self.decoder = nn.ModuleList()
         # maxpool
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         in_channels = 3
         for feature in self.features:
             self.encoder.append(nn.Conv2d(in_channels, feature, 3, padding=1))
             in_channels = feature
+        self.bottom = nn.Conv2d(self.features[-1], self.features[-1]*2, 3, padding=1)
+        # out channels = 8
+        for feature in reversed(self.features):
+            self.decoder.append(nn.Conv2d(feature*2, feature, 3, padding=1))
+            self.decoder.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        for step in self.encoder:
+            x = step(x)
+            x = self.pool(x)
+        x = self.bottom(x)
+        print('x shape:', x.size())
+        x = self.decoder[0](x)
+        print('x shape after decoder0:', x.size())
+
+        #for step in self.decoder:
+        #    x = step(x)
 
         return x
