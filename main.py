f92d5976695d3bd9b7aa44082ad6373f7fd3a673
aklopezcarbajal <aklopezcarbajal@gmail.com>
2021-12-01 11:51:03 -0500

    Re-structure code

--- main.py
+++ main.py
@@ -4,26 +4,34 @@ import torch
 import torch.nn as nn
 from torchmetrics import IoU, Accuracy
 from torch.utils.data import DataLoader, random_split
-from utils import imgDataset, imshow_mult
+from utils import imgDataset, show_example
 from models import ConvNet, UNet
 from preprocess import preprocess_images
 import matplotlib.pyplot as plt
 """
 Dataset: Underwater imagery (SUIM)
 """
+# Select model: 'unet', 'conv'
+model = 'conv'
+# Hyperparameters
+params = {
+    "model": model,
+    "learning_rate": 0.001,
+    "batch_size": 8,
+    "epochs": 100
+}
 # Create comet experiment
 experiment = Experiment(
     api_key=os.environ['API_KEY'],
     project_name="semantic-segmentation",
     workspace=os.environ['WORKSPACE'],
+    disabled=False
 )
-# Hyperparameters
-params = {
-    "learning_rate": 0.001,
-    "batch_size": 8,
-    "epochs": 10
-}
 experiment.log_parameters(params)
+
+# Check if cuda is available
+train_on_gpu = torch.cuda.is_available()
+print('Is cuda available?', 'Yes' if train_on_gpu else 'No')
 
 def prepare_data(preprocess=False):
     if preprocess:
@@ -45,25 +53,25 @@ def prepare_data(preprocess=False):
 
     return train_loader, val_loader
 
-def train_step(data, target, optimizer, criterion, train_on_gpu=False):
+def train_step(model, data, target, optimizer, criterion):
     if train_on_gpu:
         data, target = data.cuda(), target.cuda()
     target = torch.squeeze(target, dim=1)
     optimizer.zero_grad()
 
-    output = net(data)
+    output = model(data)
     loss = criterion(output, target)
     loss.backward()
     optimizer.step()
 
     return loss.item()
 
-def validation_step(data, target, criterion, metrics, train_on_gpu=False):
+def validation_step(model, data, target, criterion, metrics):
     if train_on_gpu:
         data, target = data.cuda(), target.cuda()
     target = torch.squeeze(target, dim=1)
 
-    output = net(data)
+    output = model(data)
     loss = criterion(output, target)
 
     for metric in metrics:
@@ -71,71 +79,77 @@ def validation_step(data, target, criterion, metrics, train_on_gpu=False):
 
     return loss.item()
 
-# Select model: 'unet', 'conv'
-model = 'conv'
-# Check if cuda is available
-train_on_gpu = torch.cuda.is_available()
-print('Is cuda available?', 'Yes' if train_on_gpu else 'No')
+def train_epoch(model, train_loader, optimizer, criterion):
+    model.train()
 
-train_loader, val_loader = prepare_data()
-
-if model == 'unet':
-    net = UNet()
-else:
-    net = ConvNet()
-
-# Loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=params["learning_rate"])
-
-# Metrics
-iou = IoU(num_classes=8)
-accuracy = Accuracy(num_classes=8)
-metrics = [iou, accuracy]
-
-# If cuda is available
-if train_on_gpu:
-    print('Training on GPU ...')
-    model.cuda()
-    criterion.cuda()
-    iou.cuda()
-    accuracy.cuda()
-
-train_loss, val_loss = [], []
-epochs = params["epochs"]
-for epoch in range(epochs):
-    running_loss, val_running_loss = 0., 0.
-
-    net.train()
+    running_loss = 0.
     for image, mask in train_loader:
-        running_loss += train_step(image, mask, optimizer, criterion)
+        running_loss += train_step(model, image, mask, optimizer, criterion)
+    return running_loss
 
-    net.eval()
+def validation_epoch(model, val_loader, criterion, metrics):
+    model.eval()
+
+    val_running_loss = 0.
     for image, mask in val_loader:
-        val_running_loss += validation_step(image, mask, criterion, metrics)
+        val_running_loss += validation_step(model, image, mask, criterion, metrics)
+    return val_running_loss
 
-    if epoch%5 == 0:
-        output = net(image)
-        top_class = torch.argmax(output, 1)
-        experiment.log_image(top_class[0], name='output')
-    experiment.log_metric('IoU', iou.compute())
-    experiment.log_metric('val_accuracy', accuracy.compute())
+def train(model, train_loader, val_loader):
+    # Loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=params["learning_rate"])
 
-    train_loss.append(running_loss / len(train_loader.dataset))
-    val_loss.append(val_running_loss/len(val_loader.dataset))
+    # Metrics
+    iou = IoU(num_classes=8)
+    accuracy = Accuracy(num_classes=8)
+    metrics = [iou, accuracy]
 
-    print('[epoch', epoch+1, '] Training loss: %.5f' %train_loss[-1], ' Validation loss: %.5f' %val_loss[-1])
-    print('         Accuracy: %.2f' %accuracy.compute())
+    # If cuda is available train on gpu
+    if train_on_gpu:
+        print('Training on GPU ...')
+        model.cuda()
+        criterion.cuda()
+        iou.cuda()
+        accuracy.cuda()
 
-# Show example: input image, mask and output
-net.eval()
-img_batch, mask_batch = next(iter(val_loader))
-output = torch.argmax(net(img_batch), 1)
-img, mask, pred = img_batch[1], mask_batch[1], output[1]
-imshow_mult([img, mask, pred], ['Input', 'Label', 'Prediction'])
+    train_loss, val_loss = [], []
+    for epoch in range(params["epochs"]):
+        running_loss = train_epoch(model, train_loader, optimizer, criterion)
+        val_running_loss = validation_epoch(model, val_loader, criterion, metrics)
 
-# Plots
-epochs_arr = [i+1 for i in range(epochs)]
-plt.plot(epochs_arr, train_loss, label='Training loss')
-plt.plot(epochs_arr, val_loss, label='Validation loss')
-plt.show()
+        if epoch % 5 == 0:
+            image, _ = next(iter(val_loader))
+            pred = torch.argmax(model(image), 1)
+            experiment.log_image(pred[0], name='output')
+
+        experiment.log_metric('IoU', iou.compute())
+        experiment.log_metric('val_accuracy', accuracy.compute())
+
+        train_loss.append(running_loss / len(train_loader.dataset))
+        val_loss.append(val_running_loss / len(val_loader.dataset))
+
+        print('[epoch', epoch + 1, '] Training loss: %.5f' % train_loss[-1], ' Validation loss: %.5f' % val_loss[-1])
+        print('         Accuracy: %.2f' % accuracy.compute())
+
+    # Plots
+    epochs_arr = [i + 1 for i in range(params["epochs"])]
+    plt.plot(epochs_arr, train_loss, label='Training loss')
+    plt.plot(epochs_arr, val_loss, label='Validation loss')
+    plt.show()
+
+def main():
+    # Prepare data
+    train_loader, val_loader = prepare_data()
+    # Initialize model
+    if model == 'unet':
+        net = UNet()
+    else:
+        net = ConvNet()
+    # Train model
+    train(net, train_loader, val_loader)
+    # Show prediction example: input, mask, prediction
+    show_example(net, val_loader)
+
+if __name__ == '__main__':
+    main()
