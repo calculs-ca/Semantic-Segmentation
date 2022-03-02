f87054c3c2498d8b3be6444851cfdccaf544bc17
aklopezcarbajal <aklopezcarbajal@gmail.com>
2022-03-02 11:30:19 -0500

    Modify display example function. Remove unnecessary code.

--- utils.py
+++ utils.py
@@ -3,32 +3,6 @@ import matplotlib.pyplot as plt
 import torchvision
 from torch.utils.data import Dataset
 
-def imshow(img):
-    img = torch.squeeze(img)
-
-    imshape = list(img.shape)
-    if imshape[0] == 3:         # RGB image
-        img = img.swapaxes(0, 1)
-        img = img.swapaxes(1, 2)
-        plt.imshow(img.numpy())
-    elif len(imshape) == 2:     # grayscale image
-        img = torch.squeeze(img)
-        n_classes = 7
-        plt.imshow(img.numpy(), cmap='gray', vmin=0, vmax=n_classes)
-
-# Show input image, ground truth and model output
-def imshow_mult(imgs, titles=None):
-    fig = plt.figure(figsize=(8, 3))
-    rows = 1
-    cols = len(imgs)
-    for i in range(cols):
-        fig.add_subplot(rows, cols, i+1)
-        imshow(imgs[i])
-        plt.axis('off')
-        title = 'figure'+str(i+1) if titles is None else titles[i]
-        plt.title(title)
-    plt.show()
-
 # Images dataset
 class imgDataset(Dataset):
     def __init__(self, images, masks):
@@ -40,13 +14,6 @@ class imgDataset(Dataset):
 
     def __getitem__(self, i):
         return self.images[i], self.masks[i]
-
-# Show example: input image, mask and output
-def show_seg(model, loader):
-    model.eval()
-    img_batch, mask_batch = next(iter(loader))
-    output = torch.argmax(model(img_batch), 1)
-    imshow_mult([img_batch[0], mask_batch[0], output[0]], ['Input', 'Ground truth', 'Prediction'])
 
 def visualize_seg(image, seg_pred, seg_true):
     def t(img):
@@ -70,3 +37,11 @@ def visualize_seg(image, seg_pred, seg_true):
     )
     g = torchvision.utils.make_grid([image, seg_true, seg_pred])
     return g.moveaxis(0, -1)
+
+# Show example: input image, mask and output
+def show_seg(model, loader):
+    model.eval()
+    img_batch, mask_batch = next(iter(loader))
+    viz = visualize_seg(img_batch[0], model(img_batch)[0], mask_batch[0])
+    plt.imshow(viz)
+    plt.show()
